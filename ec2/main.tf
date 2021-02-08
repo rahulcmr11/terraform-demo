3fe092a7cd7fbebff9b29f0580077588c51ecfa6
rahulranjan5 <rahulranjan5@deloitte.com>
2021-02-08 16:12:47 +0530

    update

---
+++ ec2/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+ region = "us-east-1"
+ access_key = "AKIAXQ4SBAZ4ZBUQZWRU"
+ secret_key = "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"
+}
+
+resource "aws_instance" "myweb" {
+  ami           = "ami-047a51fa27710816e"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AkuDev"
+  }
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "kilpilpande"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
