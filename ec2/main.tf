c8e64910862a072f0455327170f4c433606b448b
rahulcmr11 <41458947+rahulcmr11@users.noreply.github.com>
2021-02-09 10:41:12 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,25 +1,27 @@
-provider "aws" {
- region = "us-east-2"
- shared_credentials_file = "/home/ubuntu/rahul-demo/credentials"
- profile = "TF"
+provider "aws" {                                  ### provider is aws, can be azure , google etc.
+ region = "us-east-2"                             ### region to create aws resources.
+ shared_credentials_file = "/home/ubuntu/rahul-demo/credentials"          ### Credetials to connect aws and terraform cli stored in file
+ profile = "TF"                                                  ### profile name of saved aws credentials 
 
 }
+
+## 1st resource , will be created
 
 resource "aws_instance" "myweb" {
-  ami           = "ami-047a51fa27710816e"
-  instance_type = "t2.micro"
+  ami           = "ami-047a51fa27710816e"                   ### AMI id for ec2
+  instance_type = "t2.micro"                                ### type of ec2, large , p2 , m5 etc. 
 
   tags = {
-    Name = "demowas-int"
+    Name = "demo-web-server"                                 ## server  name, change for demo
   }
 }
-
+## 2nd resource s3 bucket
 resource "aws_s3_bucket" "b" {
-  bucket = "demo-tf-08022021"
+  bucket = "demo-tf-09022021"                                  ##bucket name , change for demo                       
   acl    = "private"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name        = "My bucket"                      ### Tags
+    Environment = "Dev"                            ### Name of envoirnment, dev prof etc
   }
 }
