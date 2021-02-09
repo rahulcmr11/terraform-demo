c6606beaa18b6f6b2a65d8d4ddbcb72168787ddc
rahulcmr11 <41458947+rahulcmr11@users.noreply.github.com>
2021-02-09 11:50:19 +0530

    Create jenkins_tf_destroy.java

---
+++ jenkins_tf_destroy.java
@@ -0,0 +1,37 @@
+import groovy.json.JsonSlurper
+@Library('github.com/releaseworks/jenkinslib') _ 
+String CODE_REPO = "https://github.com/rahulcmr11/terraform-demo.git" // clone this git repo
+node {
+
+    ws("terraform-demo-demo") {    // workspace for server
+
+        stage('checkout-git') {
+
+           checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'demo-terraform', url: CODE_REPO]]]
+
+        }
+	    
+	    stage('check_init') {  // run terraform init 
+		
+		sh '''
+			cd ec2
+			sudo terraform init 
+			
+			
+			 
+		'''    
+		println("All look good . Please run the terraform apply ")	
+        }
+	    
+	    stage('destroy_the_resources') {   // after success of above stage run the apply
+		    
+	sh '''
+	    cd ec2
+	    sudo terraform destroy -auto-approve # destroy resources  
+			 
+	  '''
+			
+        }
+	    
+        }
+        }
