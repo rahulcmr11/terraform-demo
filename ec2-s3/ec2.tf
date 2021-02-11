7b5671a29461316a86734027e8454a7d42795755
Lokesh <lokeshnaidu5862@gmail.com>
2021-02-11 13:28:16 +0530

    new code

---
+++ ec2-s3/ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {                                  ### provider is aws, can be azure , google etc.
+ region = "us-east-2"                             ### region to create aws resources.
+ shared_credentials_file = "/home/ubuntu/rahul-demo/credentials"   ### Credetials to connect aws and terraform cli stored in file
+ profile = "TF"                                   ### profile name of saved aws credentials 
+}
+
+## 1st resource EC2 , will be created
+resource "aws_instance" "myweb" {
+  ami           = "ami-01aab85a5e4a5a0fe"                   ### AMI id for ec2
+  instance_type = "t2.micro"                                ### type of ec2, large , p2 , m5 etc. 
+
+  tags = {
+    Name = "demo-web-server-terraform2"                                 ## server  name, change for demo
+  }
+}
+
---
+++ ec2-s3/s3.tf
@@ -0,0 +1,10 @@
+## 2nd resource s3 bucket
+resource "aws_s3_bucket" "b" {
+  bucket = "demo-tf-09022021-terraform3"                                  ##bucket name , change for demo                       
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"                      ### Tags
+    Environment = "Dev"                            ### Name of envoirnment, dev prof etc
+  }
+}
