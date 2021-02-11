9f3afa90c73be83e9df726d2efdb33e0bbd91c7a
rahulcmr11 <41458947+rahulcmr11@users.noreply.github.com>
2021-02-11 13:40:33 +0530

    s3 updated

--- ec2-s3/s3.tf
+++ ec2-s3/s3.tf
@@ -1,8 +1,4 @@
-provider "aws" {                                  ### provider is aws, can be azure , google etc.
- region = "us-east-2"                             ### region to create aws resources.
- shared_credentials_file = "/home/ubuntu/rahul-demo/credentials"   ### Credetials to connect aws and terraform cli stored in file
- profile = "TF"                                   ### profile name of saved aws credentials 
-}
+
 
 ## 2nd resource s3 bucket
 resource "aws_s3_bucket" "b" {
