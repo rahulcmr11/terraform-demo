0f4b45e3e60cbd8c844dc753730b77725dee0e80
rahulcmr11 <41458947+rahulcmr11@users.noreply.github.com>
2021-02-11 16:21:19 +0530

    Update s3.tf

--- ec2-s3/s3.tf
+++ ec2-s3/s3.tf
@@ -2,7 +2,7 @@
 
 ## 2nd resource s3 bucket
 resource "aws_s3_bucket" "b" {
-  bucket = "demo-tf-09022021-terraform4"                                  ##bucket name , change for demo                       
+  bucket = "demo-tf-11022021-terraform"                                  ##bucket name , change for demo                       
   acl    = "private"
 
   tags = {
